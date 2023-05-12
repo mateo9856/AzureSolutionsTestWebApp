60f94dd7a693225d66b29b1ccf79a9de79b70797
Mateusz Magdziak <mateusz.magdziak@dxc.com>
2023-05-12 09:29:40 +0200

    Add project files.

---
+++ AzureSolutionsTestWebApp/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureSolutionsTestWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult ReturnRandomNumArray()
+        {
+            var random = new Random();
+            int[] arr = new int[random.Next(3, 10)];
+
+            for(int i = 0; i< arr.Length; i++)
+            {
+                arr[i] = random.Next(0, 20);
+            }
+
+            return Ok(arr);
+        }
+
+    }
+}
